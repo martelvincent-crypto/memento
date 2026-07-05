27e708341a0d34c560e1ba1b5edeff34c370b5d7
martelvincent-crypto <martel.vincent@gmail.com>
2026-07-05 15:18:32 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'memento-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }));
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; })
+                            .map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return r || fetch(e.request).then(function (resp) {
+        const copy = resp.clone();
+        caches.open(CACHE).then(function (c) { c.put(e.request, copy); }).catch(function () {});
+        return resp;
+      }).catch(function () { return caches.match('./index.html'); });
+    })
+  );
+});
